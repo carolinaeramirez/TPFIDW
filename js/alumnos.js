2c7597b6a98086248aff60c4bfde9ac94967bc43
Dardo Flores Blanc <drfb87@gmail.com>
2023-05-26 19:14:12 -0300

    agrego dni en html alumnos

--- js/alumnos.js
+++ js/alumnos.js
@@ -4,12 +4,13 @@ console.log(JSON.parse(storedData));
 var datos = JSON.parse(storedData);
 var tablaHTML = "<table>";
 tablaHTML +=
-  "<tr><th>Nombre</th><th>Apellido</th><th>Fecha de nacimiento</th><th>Nacionalidad</th><th>Correo electronico</th><th>Celular</th></tr>";
+  "<tr><th>Nombre</th><th>Apellido</th><th>DNI</th><th>Fecha de nacimiento</th><th>Nacionalidad</th><th>Correo electronico</th><th>Celular</th></tr>";
 for(var i = 0; i< datos.length; i++){
     console.log(datos.length)
     tablaHTML+= "<tr>";
     tablaHTML+= "<td>" +datos[i].nombre + "</td>";
     tablaHTML+= "<td>" +datos[i].apellido + "</td>";
+    tablaHTML+= "<td>" +datos[i].dni + "</td>";
     tablaHTML+= "<td>" +datos[i].fechaNacimiento + "</td>";
     tablaHTML+= "<td>" +datos[i].nacionalidad + "</td>";
     tablaHTML+= "<td>" +datos[i].email + "</td>";
