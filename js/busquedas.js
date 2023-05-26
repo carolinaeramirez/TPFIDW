94896ea1682396a2fb493229915c66fac3233af6
carolinaeramirez <crmz4836@gmail.com>
2023-05-25 21:44:06 -0300

    agrego la pagina administracion y la busqueda

---
+++ js/busquedas.js
@@ -0,0 +1,21 @@
+function buscar() {
+  document
+    .getElementById("busqueda-form")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+      const datoIngresado = document.getElementById("datoBusc").value; //capturamos el dato ingresado en el input de busqueda
+      // traemos los datos guardados en localStorage
+      var storedData = localStorage.getItem("datos");
+      //parseamos esos datos
+      var datosAlmacenados = JSON.parse(storedData);
+      // filtramos los datos parseados y le decimos que muestre los que incluyan el dato ingresado
+      var resultados = datosAlmacenados.filter(function (item) {
+        var nombreItem = item.nombre.toLowerCase();
+        return nombreItem.includes(datoIngresado);
+      });
+      //convertimos los resultados en objeto para poder mostrarlos
+      result = JSON.stringify(resultados);
+      console.log(result);
+      return resultados;
+    });
+}
